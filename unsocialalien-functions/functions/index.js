3132ef26f8a1412a1a74fdb71df0c7b705d880de
AnushaChengi <anushachengi@gmail.com>
2020-01-13 23:32:46 +0530

    1)Created "getposts" request
    Files Changed:
    .firebaserc
    .gitignore
    .gitignore
    firebase.json
    index.js
    misc.xml
    modules.xml
    package-lock.json
    package.json
    unsocialalien-functions.iml
    vcs.xml

---
+++ unsocialalien-functions/functions/index.js
@@ -0,0 +1,25 @@
+const functions = require('firebase-functions');
+
+const admin = require('firebase-admin')
+admin.initializeApp()
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+exports.helloWorld = functions.https.onRequest((request, response) => {
+    response.send("Alien works for Firebase");
+});
+
+exports.getPosts = functions.https.onRequest((req, res) => {
+    admin.firestore().collection('alien-posts').get()
+        .then(data => {
+            let posts = [];
+            data.forEach(doc => {
+                posts.push(doc.data())
+            })
+            return res.json(posts)
+        })
+        .catch(err => {
+            console.error(err)
+        })
+})
