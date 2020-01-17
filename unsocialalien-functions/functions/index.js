2f59016de58902618f68543cc053399b9c3305c9
AnushaChengi <anushachengi@gmail.com>
2020-01-17 18:39:08 +0530

    user crredentials validation and Login Route

--- unsocialalien-functions/functions/index.js
+++ unsocialalien-functions/functions/index.js
@@ -63,6 +63,21 @@ app.post('/post', (req, res) => {
         })
 })
 
+const xisEmpty = (string) => {
+    if(string.trim() === '') {
+        return true
+    }else return false
+}
+const isEmail = (email) => {
+    const emailRegEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if(email.match(emailRegEx)){
+        return true
+    }else return false
+
+}
+
+
+
 app.post('/signUp', (req, res) => {
     const newUser = {
         email: req.body.email,
@@ -70,7 +85,22 @@ app.post('/signUp', (req, res) => {
         confirmPassword: req.body.confirmPassword,
         handle: req.body.handle,
     }
-    let token,userId;
+    let errors = {}
+    if(isEmpty(newUser.email)){
+        errors.email = 'Field must not be empty '
+    }else if(!isEmail(newUser.email)){
+        errors.email = 'Must be a valid email address'
+    }
+
+    if(isEmpty(newUser.password)) errors.password = 'Field must not be empty'
+    if(newUser.password !== newUser.confirmPassword) errors.confirmPassword = 'Must match password'
+    if(isEmpty(newUser.handle)) errors.handle = 'Field must not be empty'
+
+    if(Object.keys(errors).length > 0) {
+        res.status(400).json(errors)
+    }
+
+    let token, userId;
     db.doc(`/users/${newUser.handle}`).get()
         .then(doc => {
             if (doc.exists) {
@@ -87,28 +117,53 @@ app.post('/signUp', (req, res) => {
         })
         .then((idToken) => {
             token = idToken
-            const userCredentials =  {
+            const userCredentials = {
                 handle: newUser.handle,
-                email:newUser.email,
-                createdAt : new Date().toISOString(),
+                email: newUser.email,
+                createdAt: new Date().toISOString(),
                 userId
             }
             db.doc(`users/${newUser.handle}`).set(userCredentials)
         })
-        .then(()=>{
+        .then(() => {
             return res.status(201).json({token})
         })
-        .catch((err)=> {
+        .catch((err) => {
             console.log(err)
-            if(err.code === 'auth/email-already-in-use'){
+            if (err.code === 'auth/email-already-in-use') {
                 return res.status(400).json({email: 'email already in use'})
-            }
-            else{
+            } else {
                 return res.status(500).json({error: err.code})
             }
         })
 
 })
 
+app.post(`/logIn`,(req,res) =>{
+    const logInUser = {
+        email : req.body.email,
+        password: req.body.password
+    }
+    let errors ={}
+
+    if(isEmpty(logInUser.email)) return errors.email =  'Field must not be empty '
+    if(isEmpty(logInUser.password)) return errors.password =  'Field must not be empty '
+
+    if(Object.keys(errors).length > 0) return res.status(400).json(errors)
+    firebase.auth().signInWithEmailAndPassword(logInUser.email, logInUser.password)
+        .then(data =>{
+            return data.user.getIdToken()
+        })
+        .then(token =>{
+            return res.json({token})
+        })
+        .catch(err =>{
+            console.log(err.code)
+            if(err.code === 'auth/wrong-password'){
+                return res.status(403).json({error : 'Wrong password credentials' })
+            }else return res.status(500).json({error :err.code})
+        })
+
+})
 
 exports.api = functions.https.onRequest(app)
