9e20941afe43ba92e33524a3f23011a53cfabfe5
AnushaChengi <anushachengi@gmail.com>
2020-01-18 21:30:37 +0530

    files refactoring and organizing

---
+++ unsocialalien-functions/functions/handlers/posts.js
@@ -0,0 +1,40 @@
+const {db} = require('../util/admin');
+
+exports.getAllPosts = (req, res) => {
+  db.collection('alien-posts')
+    .get()
+    .then(data => {
+      let posts = [];
+      data.forEach(doc => {
+        posts.push({
+          postId: doc.id,
+          body: doc.data().body,
+          userHandle: doc.data().userHandle,
+          createdAt: doc.data().createdAt,
+        });
+      });
+      return res.json(posts);
+    })
+    .catch(err => {
+      console.error(err);
+    });
+};
+
+exports.postOnePost = (req, res) => {
+  if (req.body.body.trim() === '') {
+    return res.status(400).json({body: 'Body shouldnt be empty'});
+  }
+  const newPost = {
+    body: req.body.body,
+    userHandle: req.user.handle,
+    createdAt: admin.firestore.Timestamp.fromDate(new Date()),
+  };
+  db.collection('alien-posts')
+    .add(newPost)
+    .then(doc => {
+      res.json(`this ${doc.id} is succesfull`);
+    })
+    .catch(err => {
+      res.status(500).json({error: 'server not working'});
+    });
+};
---
+++ unsocialalien-functions/functions/util/firebaseConfig.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apiKey: 'AIzaSyDwJJX0qaLg9sHSzLPFF4TKsbUK5L_c1VA',
+  authDomain: 'unsocial-alien.firebaseapp.com',
+  databaseURL: 'https://unsocial-alien.firebaseio.com',
+  projectId: 'unsocial-alien',
+  storageBucket: 'unsocial-alien.appspot.com',
+  messagingSenderId: '362380376412',
+  appId: '1:362380376412:web:0a6ecea50d564d6710e999',
+  measurementId: 'G-P8DX3K4MRV',
+};
