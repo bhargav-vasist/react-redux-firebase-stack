6a4c02d32aacb6912ef1924996290b00bade9aaa
AnushaChengi <anushachengi@gmail.com>
2020-01-20 21:58:15 +0530

    dbSchema file updated

---
+++ unsocialalien-functions/dbSchema.js
@@ -0,0 +1,38 @@
+let db = {
+  users: [
+    {
+      userId: 'uHpBfXHCyycdv3DNDCIGzWx0vZD3',
+      email: 'anusha@gmail.com',
+      handle: 'Anusha',
+      createdAt: '2020-01-20T10:39:16.045Z',
+      imageUrl:
+        'https://firebasestorage.googleapis.com/v0/b/unsocial-alien.appspot.com/o/no-image.png?alt=media',
+      bio: 'Hey , you can write you cool bio here :)',
+      website: 'https://website.com',
+      location: 'India, bangalore',
+    },
+  ],
+  posts: [
+    {
+      userHandle: 'Anusha',
+      body: 'This is a sample post',
+      createdAt: '2020-01-20T10:39:16.045Z',
+      likeCount: 10,
+      commentCount: 3,
+    },
+  ],
+};
+const userDetails = {
+  // Redux Data
+  credentials: {
+    userId: 'uHpBfXHCyycdv3DNDCIGzWx0vZD3',
+    email: 'anusha@gmail.com',
+    handle: 'Anusha',
+    createdAt: '2020-01-20T10:39:16.045Z',
+    imageUrl:
+      'https://firebasestorage.googleapis.com/v0/b/unsocial-alien.appspot.com/o/no-image.png?alt=media',
+    bio: 'Hey , you can write you cool bio here :)',
+    website: 'https://website.com',
+    location: 'India, bangalore',
+  },
+};
