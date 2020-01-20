536738dbeab472e450f9b5b7655c08cfed6ee8a1
AnushaChengi <anushachengi@gmail.com>
2020-01-20 22:02:02 +0530

    dbSchema file updated

--- unsocialalien-functions/dbSchema.js
+++ unsocialalien-functions/dbSchema.js
@@ -2,8 +2,8 @@ let db = {
   users: [
     {
       userId: 'uHpBfXHCyycdv3DNDCIGzWx0vZD3',
-      email: 'anusha@gmail.com',
-      handle: 'Anusha',
+      email: 'user@gmail.com',
+      handle: 'user',
       createdAt: '2020-01-20T10:39:16.045Z',
       imageUrl:
         'https://firebasestorage.googleapis.com/v0/b/unsocial-alien.appspot.com/o/no-image.png?alt=media',
@@ -14,7 +14,7 @@ let db = {
   ],
   posts: [
     {
-      userHandle: 'Anusha',
+      userHandle: 'user',
       body: 'This is a sample post',
       createdAt: '2020-01-20T10:39:16.045Z',
       likeCount: 10,
@@ -26,8 +26,8 @@ const userDetails = {
   // Redux Data
   credentials: {
     userId: 'uHpBfXHCyycdv3DNDCIGzWx0vZD3',
-    email: 'anusha@gmail.com',
-    handle: 'Anusha',
+    email: 'user@gmail.com',
+    handle: 'user',
     createdAt: '2020-01-20T10:39:16.045Z',
     imageUrl:
       'https://firebasestorage.googleapis.com/v0/b/unsocial-alien.appspot.com/o/no-image.png?alt=media',
@@ -35,4 +35,14 @@ const userDetails = {
     website: 'https://website.com',
     location: 'India, bangalore',
   },
+  likes: [
+    {
+      userHandle: 'user',
+      postId: 'hh705oWfWucVzGbHH2pa',
+    },
+    {
+      userHandle: 'user',
+      postId: 'jj705oWfWucVGbHH4pa',
+    },
+  ],
 };
